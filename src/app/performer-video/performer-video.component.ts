522550261a4db26b42cd9d97d1a959e53d41d4bc
Rob Ridings <rob.ridings@gmail.com>
2019-05-31 11:50:55 -0400

    Added voteing frontend

--- src/app/performer-video/performer-video.component.ts
+++ src/app/performer-video/performer-video.component.ts
@@ -5,6 +5,8 @@ import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'app/auth/auth.service';
 import { VideosService } from 'app/services/videos.service';
 import { Video } from 'app/model/video';
+import { Role } from 'app/model/role';
+import { User } from 'app/model/user';
 
 @Component({
   selector: 'performer-video',
@@ -15,10 +17,20 @@ export class PerformerVideoComponent implements OnInit {
 
   currentVideoUrl$ : Observable<string>;
 
-  constructor(public videosService: VideosService, public auth: AuthService, private route: ActivatedRoute){
+  vote : string;
+  
+  constructor(public videosService: VideosService, public authService: AuthService, private route: ActivatedRoute){
   }
 
   ngOnInit() {
     this.currentVideoUrl$ = this.videosService.currentVideoUrl$;
+  }
+  
+  getCurrentUser() : User {
+    return this.authService.user;
+  }
+  
+  onCastVote(role : Role): void {
+    console.log("User - " + role.user +  " Vote - " + this.vote);
   }
 }
